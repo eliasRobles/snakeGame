6ac4439ecd536dc2926d612f96fa900b41eb68bf
eliasRobles <82739687+eliasRobles@users.noreply.github.com>
2022-02-02 12:14:27 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+# Press the green button in the gutter to run the script.
+from SnakeGame import SnakeGame
+
+if __name__ == '__main__':
+    snake_game = SnakeGame()
+    snake_game.jugar()
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
